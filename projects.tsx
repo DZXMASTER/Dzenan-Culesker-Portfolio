af780b05250d30b0ca62a144cc154f8f2bc7f81f
Dzenan Culesker <dzenanculesker@gmail.com>
2024-10-13 22:04:12 -0400

    Add files via upload

---
+++ projects.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
+
+  return (
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
+      <motion.p 
+        initial={{ opacity: 1 }}
+        whileInView={{ opacity: 0 }}
+        transition={{ delay: 5 }}
+        className="flex w-full justify-center"
+        >
+          (Hint: Click Image for Project)
+      </motion.p>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}

27e42e7a05cf0ca41bcfd86fc5cb7d00d1e35b97
Dzenan Culesker <dzenanculesker@gmail.com>
2024-10-13 22:10:28 -0400

    Add files via upload

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
@@ -12,6 +13,14 @@ export default function Projects() {
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
+      <motion.p 
+        initial={{ opacity: 1 }}
+        whileInView={{ opacity: 0 }}
+        transition={{ delay: 5 }}
+        className="flex w-full justify-center"
+        >
+          (Hint: Click Image for Project)
+      </motion.p>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
